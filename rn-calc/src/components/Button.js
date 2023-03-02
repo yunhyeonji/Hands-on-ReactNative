edcda2bf34563853626b6355fef0d126956c3226
DESKTOP-VCB2MUO\owner <yhj@danusys.com>
2023-03-02 17:19:24 +0900

    onPress관련 문법

--- rn-calc/src/components/Button.js
+++ rn-calc/src/components/Button.js
@@ -1,8 +1,45 @@
-import {Text} from 'react-native';
+import {Pressable, Text, TouchableHighlight, TouchableOpacity} from 'react-native';
 import PropTypes from 'prop-types';
 
 const Button = ({title}) => {
-    return <Text>{title}</Text>;
+    return (
+        <Pressable
+            onPressIn={() => console.log("in!")}
+            onPressOut={() => console.log("out!")}
+            onPress={() => console.log("onPress!")}
+            onLongPress={() => console.log("Long!")}
+            delayLongPress={2000}
+            style={(data) => {
+               // console.log(data);
+                return [
+                    {backgroundColor: 'green'},
+                    data.pressed && {backgroundColor:'orange',opacity:0.3},
+                ]
+            }}
+        >
+            <Text>{title}</Text>
+        </Pressable>
+    );
+
+    // return (
+    //     <TouchableOpacity 
+    //         onPress={() => console.log("click!")}
+    //         style={{backgroundColor:'green'}}
+    //         // underlayColor={'orange'}
+    //     >
+    //         <Text>{title}</Text>
+    //     </TouchableOpacity >
+    // );
+
+    // return (
+    //     <TouchableHighlight
+    //         onPress={() => console.log("click!")}
+    //         style={{backgroundColor:'green'}}
+    //         // underlayColor={'orange'}
+    //     >
+    //         <Text>{title}</Text>
+    //     </TouchableHighlight>
+    // );
 };
 
 Button.defaultProps = {
