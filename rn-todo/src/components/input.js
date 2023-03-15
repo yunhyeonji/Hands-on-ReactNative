bec37c9258aacbcd03b54dd7d8f07811ac6a4987
DESKTOP-VCB2MUO\owner <yhj@danusys.com>
2023-03-15 15:03:04 +0900

    구조분해 할당 사용-코드가 깔끔해짐

--- rn-todo/src/components/input.js
+++ rn-todo/src/components/input.js
@@ -11,18 +11,13 @@ export const ReturnKeyTypes = {
   NEXT: 'next',
 };
 
-const Input = ({
-  title,
-  placeholder,
-  keyboardType,
-  returnKeyType,
-  secureTextEntry,
-}) => {
+const Input = ({ title, placeholder, ...props }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
 
       <TextInput
+        {...props}
         style={styles.input}
         // null 이거나 undefind일 때 title을 넣어준다. placeholder={''}이렇게 라도 보내게 되면 placeholder로 인식함
         placeholder={placeholder ?? title}
@@ -33,14 +28,9 @@ const Input = ({
         autoCapitalize={'none'}
         // 글씨가 오타가 났을때 확인 후 자동 수정을 하게 됨 =>  false일 시 수정 안됨
         autoCorrect={false}
-        // 이메일 작성에 특화된 키보드 타입 설정
-        keyboardType={keyboardType}
-        returnKeyType={returnKeyType}
         textContentType={'none'}
-        // 텍스트를 특수문자로 변경
-        secureTextEntry={secureTextEntry}
         // 키보드 색상 변경 가능 (IOS)
-        // keyboardAppearance={'dark'}
+        keyboardAppearance={'dark'}
       />
     </View>
   );
@@ -53,9 +43,6 @@ Input.defaultProps = {
 Input.propTypes = {
   title: PropTypes.string,
   placeholder: PropTypes.string,
-  keyboardType: PropTypes.oneOf(Object.values(keyboardTypes)),
-  returnKeyType: PropTypes.oneOf(Object.values(ReturnKeyTypes)),
-  secureTextEntry: PropTypes.bool,
 };
 
 const styles = StyleSheet.create({
