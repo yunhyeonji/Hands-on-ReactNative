53758eda1259b0d4963342c01e37489e91677707
DESKTOP-VCB2MUO\owner <yhj@danusys.com>
2023-05-02 16:01:59 +0900

    @react-navigation/native 사용준비

--- rn-todo/src/App.js
+++ rn-todo/src/App.js
@@ -1,23 +1,15 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import { WHITE } from './Color';
 import SigninScreen from './screens/SigninScreen';
 
 const App = () => {
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
       <StatusBar style="dark" />
       <SigninScreen />
       {/* <TestAvoid></TestAvoid> */}
-    </View>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: WHITE,
-  },
-});
 
 export default App;
