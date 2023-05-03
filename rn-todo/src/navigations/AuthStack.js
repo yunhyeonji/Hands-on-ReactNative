8a4cc47d8a6b3acf69dc6e1fedab3ec984b56e3c
DESKTOP-VCB2MUO\owner <yhj@danusys.com>
2023-05-03 15:31:05 +0900

    headerTItle 사용해 보자구요

--- rn-todo/src/navigations/AuthStack.js
+++ rn-todo/src/navigations/AuthStack.js
@@ -1,24 +1,43 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignInScreen from '../screens/SignInScreen';
 import ListScreen from '../screens/ListScreen';
-import { WHITE } from '../Color';
+import { PRIMARY, WHITE } from '../Color';
+import { Pressable, Text } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 const AuthStack = () => {
   return (
     <Stack.Navigator
-      initialRouteName={'SignIn'}
+      initialRouteName={'Home'}
       screenOptions={{
         contentStyle: { backgroundColor: WHITE },
+        headerTitleAlign: 'center',
+        headerTintColor: PRIMARY.DEFAULT,
+        headerTitleStyle: { fontWeight: '700' },
+        title: 'TODO List',
       }}
     >
-      <Stack.Screen name={'SignIn'} component={SignInScreen} />
       <Stack.Screen
         name={'List'}
         component={ListScreen}
         // options={{
-        //   contentStyle: { backgroundColor: 'lavender', borderRadius: 30 },
+        //   title: 'TODO List',
+        // }}
+      />
+      <Stack.Screen
+        name={'Home'}
+        component={SignInScreen}
+        // options={{
+        //   title: 'TODO List',
+        //   headerTitle: (props) => {
+        //     console.log(props);
+        //     return (
+        //       <Pressable onPress={() => console.log('header')}>
+        //         <Text style={{ color: props.tintColor }}>{props.children}</Text>
+        //       </Pressable>
+        //     );
+        //   },
         // }}
       />
     </Stack.Navigator>
