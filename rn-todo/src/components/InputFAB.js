27c571dc8ce11a096481ee43c8b997bf238efcf0
yunHyeonji <dbsguswl4117@naver.com>
2023-06-10 20:48:26 +0900

    Animated.spring 이용

--- rn-todo/src/components/InputFAB.js
+++ rn-todo/src/components/InputFAB.js
@@ -6,7 +6,7 @@ import {
   TextInput,
   View,
   useWindowDimensions,
-  Animated
+  Animated,
 } from 'react-native';
 import { BLACK, PRIMARY, WHITE } from '../Color';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
@@ -23,29 +23,44 @@ const InputFAB = () => {
   const [keyboardHeight, setKeyboardHeight] = useState(BOTTOM);
 
   const inputWidth = useRef(new Animated.Value(BUTTON_WIDTH)).current;
+  const buttonRotation = useRef(new Animated.Value(0)).current;
+  const spin = buttonRotation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '315deg'],
+  });
 
   // 창 켜기
   const open = () => {
     setIsOpened(true);
-    Animated.timing(inputWidth,{
+    Animated.timing(inputWidth, {
       toValue: windowWidth - 20,
       useNativeDriver: false,
-      duration: 300 // 애니메이션이 동작하는 시간
+      duration: 300, // 애니메이션이 동작하는 시간
     }).start(() => {
       inputRef.current.focus();
-    })
+    });
+    Animated.spring(buttonRotation, {
+      toValue: 1,
+      useNativeDriver: false,
+      bounciness: 20,
+    }).start();
   };
 
   // 창 끄기
   const close = () => {
     setIsOpened(false);
-    Animated.timing(inputWidth,{
+    Animated.timing(inputWidth, {
       toValue: BUTTON_WIDTH,
       useNativeDriver: false,
-      duration: 300 // 애니메이션이 동작하는 시간
+      duration: 300, // 애니메이션이 동작하는 시간
     }).start(() => {
       inputRef.current.blur();
-    })
+    });
+    Animated.spring(buttonRotation, {
+      toValue: 0,
+      useNativeDriver: false,
+      bounciness: 20,
+    }).start();
   };
 
   // 버튼을 클릭했을 때 함수 실행
@@ -53,18 +68,20 @@ const InputFAB = () => {
 
   // ios에서만 사용하는 함수
   useEffect(() => {
-    const show = Keyboard.addListener('keyboardWillShow', (e) => {
-      setKeyboardHeight(e.endCoordinates.height + BOTTOM);
-    });
-    const hide = Keyboard.addListener('keyboardWillHide', () => {
-      setKeyboardHeight(BOTTOM);
-    });
+    if (Platform.OS === 'ios') {
+      const show = Keyboard.addListener('keyboardWillShow', (e) => {
+        setKeyboardHeight(e.endCoordinates.height + BOTTOM);
+      });
+      const hide = Keyboard.addListener('keyboardWillHide', () => {
+        setKeyboardHeight(BOTTOM);
+      });
 
-    // 정리함수 , useEffect함수로 이벤트를 만들때, 꼭 정리함수를 통해서 정리해줘야 함
-    return () => {
-      show.remove();
-      hide.remove();
-    };
+      // 정리함수 , useEffect함수로 이벤트를 만들때, 꼭 정리함수를 통해서 정리해줘야 함
+      return () => {
+        show.remove();
+        hide.remove();
+      };
+    }
   }, []);
 
   return (
@@ -73,7 +90,11 @@ const InputFAB = () => {
         style={[
           styles.container,
           styles.shadow,
-          { bottom: keyboardHeight, alignItems : 'flex-start', width: inputWidth},
+          {
+            bottom: keyboardHeight,
+            alignItems: 'flex-start',
+            width: inputWidth,
+          },
         ]}
       >
         <TextInput
@@ -88,19 +109,25 @@ const InputFAB = () => {
           returnKeyType="done"
           onBlur={close}
         ></TextInput>
-
       </Animated.View>
 
-      <Pressable
-        onPress={onPressButton}
-        style={({ pressed }) => [
+      <Animated.View
+        style={[
           styles.container,
-          {bottom: keyboardHeight},
-          pressed && { backgroundColor: PRIMARY.DARK },
+          { bottom: keyboardHeight, transform: [{ rotate: spin }] },
         ]}
       >
-        <MaterialCommunityIcons name="plus" size={24} color={WHITE} />
-      </Pressable>
+        <Pressable
+          onPress={onPressButton}
+          style={({ pressed }) => [
+            styles.container,
+            { right: 0 },
+            pressed && { backgroundColor: PRIMARY.DARK },
+          ]}
+        >
+          <MaterialCommunityIcons name="plus" size={24} color={WHITE} />
+        </Pressable>
+      </Animated.View>
     </>
   );
 };
@@ -108,7 +135,6 @@ const InputFAB = () => {
 const styles = StyleSheet.create({
   container: {
     position: 'absolute',
-    bottom: BOTTOM,
     right: 10,
     width: BUTTON_WIDTH,
     height: BUTTON_WIDTH,
@@ -124,14 +150,14 @@ const styles = StyleSheet.create({
   },
   // ios와 android 에서 사용하는 그림자 컴포넌트가 다름!!
   shadow: {
-    shadowColor:BLACK,
+    shadowColor: BLACK,
     ...Platform.select({
-      ios:{
-        shadowOffset: {width:2, height: 4},
+      ios: {
+        shadowOffset: { width: 2, height: 4 },
         shadowOpacity: 0.5,
         shadowRadius: 5,
       },
-      android:{ elevation: 5},
+      android: { elevation: 5 },
     }),
   },
 });
