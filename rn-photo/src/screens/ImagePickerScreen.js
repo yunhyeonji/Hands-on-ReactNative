3297c91179d92cffc3dc98ea56b9a45aacb08fdb
DESKTOP-VCB2MUO\owner <yhj@danusys.com>
2023-08-14 17:06:32 +0900

    사진 선택하기

--- rn-photo/src/screens/ImagePickerScreen.js
+++ rn-photo/src/screens/ImagePickerScreen.js
@@ -15,8 +15,12 @@ import {
   StyleSheet,
   View,
   useWindowDimensions,
+  Platform,
 } from 'react-native';
 import * as MediaLibrary from 'expo-media-library';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { PRIMARY } from '../colors';
+import { BlurView } from 'expo-blur';
 
 const initListInfo = {
   endCursor: '',
@@ -45,6 +49,7 @@ const ImagePickerScreen = () => {
   const width = useWindowDimensions().width / 3;
   const [photos, setPhotos] = useState([]);
   const listInfo = useRef(initListInfo);
+  const [selectedPhotos, setSelectedPhotos] = useState([]);
 
   // 이미지 목록 초기화 하기
   const [refreshing, setRefreshing] = useState(false);
@@ -88,17 +93,49 @@ const ImagePickerScreen = () => {
     });
   });
 
-  console.log(photos.length);
+  // 선택한 이미지가 배열안에 포함되어있는지 확인하는 함수
+  const isSelectedPhoto = (photo) => {
+    return selectedPhotos.findIndex((item) => item.id === photo.id) > -1;
+  };
+  //   이미지를 선택하기
+  const togglePhoto = (photo) => {
+    const isSelected = isSelectedPhoto(photo);
+    setSelectedPhotos((prev) =>
+      isSelected
+        ? prev.filter((item) => item.id !== photo.id)
+        : [...prev, photo]
+    );
+  };
+
   return (
     <View style={styles.container}>
       <FlatList
         style={styles.list}
         data={photos}
-        renderItem={({ item }) => (
-          <Pressable style={{ width, height: width }}>
-            <Image source={{ uri: item.uri }} style={styles.photo} />
-          </Pressable>
-        )}
+        renderItem={({ item }) => {
+          const isSelected = isSelectedPhoto(item);
+          return (
+            <Pressable
+              onPress={() => togglePhoto(item)}
+              style={{ width, height: width }}
+            >
+              <Image source={{ uri: item.uri }} style={styles.photo} />
+              {/* 이미지item이 선택되어 있을때, 사진을 blur처리하고 , 이미지 위에 체크아이콘을 띄운다 */}
+              {isSelected && (
+                <BlurView
+                  intensity={Platform.select({ ios: 10, android: 80 })}
+                  style={[StyleSheet.absoluteFillObject, styles.checkIcon]}
+                >
+                  <MaterialCommunityIcons
+                    name="check-circle"
+                    size={40}
+                    color={PRIMARY.DEFAULT}
+                  />
+                </BlurView>
+              )}
+            </Pressable>
+          );
+        }}
         numColumns={3}
         // FlatList 컴포넌트의 한 열에 렌더링하는 양
         // FlatList 컴포넌트를 세로로 사용할 때만 적용 가능
@@ -122,6 +159,7 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   photo: { width: '100%', height: '100%' },
+  checkIcon: { justifyContent: 'center', alignItems: 'center' },
 });
 
 export default ImagePickerScreen;
