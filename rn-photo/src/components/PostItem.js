0a472d6e7a354e2c79169467473ae15d1821a921
DESKTOP-VCB2MUO\owner <yhj@danusys.com>
2023-09-01 13:56:02 +0900

    postItem 컴포넌트 만들기

---
+++ rn-photo/src/components/PostItem.js
@@ -0,0 +1,73 @@
+import { View, Text, StyleSheet, useWindowDimensions } from 'react-native';
+import PropTypes from 'prop-types';
+import { memo } from 'react';
+import ImageSwiper from './ImageSwiper';
+import FastImage from './FastImage';
+import { PRIMARY, WHITE } from '../colors';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const PostItem = memo(({ post }) => {
+  const width = useWindowDimensions().width;
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        {/* USER */}
+        <FastImage
+          source={{ uri: post.user.photoURL }}
+          style={styles.profilePhoto}
+        />
+        <Text style={styles.nickname}>{post.user.displayName}</Text>
+      </View>
+
+      <View style={{ width, height: width }}>
+        {/* PHOTOS */}
+        <ImageSwiper photos={post.photos} />
+      </View>
+
+      <View style={styles.location}>
+        {/* LOCATION */}
+        <MaterialCommunityIcons
+          name="map-marker"
+          size={24}
+          color={PRIMARY.DEFAULT}
+        />
+        <Text>{post.location}</Text>
+      </View>
+
+      <Text style={styles.text}>{post.text}</Text>
+    </View>
+  );
+});
+PostItem.displayName = 'PostItem';
+
+PostItem.propTypes = {
+  post: PropTypes.object,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 10,
+    backgroundColor: WHITE,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    paddingBottom: 10,
+  },
+  profilePhoto: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+  nickname: { paddingHorizontal: 10, fontWeight: '600' },
+  location: {
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  text: { paddingHorizontal: 10 },
+});
+
+export default PostItem;
--- rn-photo/src/screens/ListScreen.js
+++ rn-photo/src/screens/ListScreen.js
@@ -1,7 +1,26 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { WHITE } from '../colors';
 import { useEffect } from 'react';
 import { getPosts } from '../api/post';
+import PostItem from '../components/PostItem';
+
+const post = {
+  createdTs: 1693392074069,
+  id: 'nCMJFusVdqhynzGguVro',
+  location: '대한민국 경기도 광명시',
+  photos: [
+    'https://firebasestorage.googleapis.com/v0/b/rn-photo-76754.appspot.com/o/zwQ57ZYjnfOeH7OxTFlOyCeRzs53%2Fs-31.jpeg.jpg?alt=media&token=5cbc7ac2-55a6-4bee-adde-29f70b1ed640',
+    'https://firebasestorage.googleapis.com/v0/b/rn-photo-76754.appspot.com/o/zwQ57ZYjnfOeH7OxTFlOyCeRzs53%2Fs-34.jpeg.jpg?alt=media&token=d14b0c2f-84ed-4636-aff0-9a38336906e3',
+    'https://firebasestorage.googleapis.com/v0/b/rn-photo-76754.appspot.com/o/zwQ57ZYjnfOeH7OxTFlOyCeRzs53%2Fs-37.jpeg.jpg?alt=media&token=db7f0c89-0093-4654-ac22-28014e705612',
+  ],
+  text: 'test7',
+  user: {
+    displayName: '닉네임',
+    photoURL:
+      'https://firebasestorage.googleapis.com/v0/b/rn-photo-76754.appspot.com/o/zwQ57ZYjnfOeH7OxTFlOyCeRzs53%2Fs-41.jpeg.jpg?alt=media&token=d0fe15c8-297c-4b94-92a7-07bf66223834',
+    uid: 'zwQ57ZYjnfOeH7OxTFlOyCeRzs53',
+  },
+};
 
 const ListScreen = () => {
   useEffect(() => {
@@ -12,7 +31,8 @@ const ListScreen = () => {
   }, []);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>ListScreen</Text>
+      {/* <Text style={styles.title}>ListScreen</Text> */}
+      <PostItem post={post}></PostItem>
     </View>
   );
 };
