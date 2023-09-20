dccd4f1aee1c312517ebc06c914113beccc3ee84
DESKTOP-VCB2MUO\owner <yhj@danusys.com>
2023-09-20 13:56:42 +0900

    액션 시트로 수정 삭제 버튼 만들기

--- rn-photo/src/App.js
+++ rn-photo/src/App.js
@@ -2,15 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import Navigation from './navigations';
 import { LogBox } from 'react-native';
 import { UserProvider } from './contexts/UserContext';
+import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 
 const App = () => {
   LogBox.ignoreLogs(['AsyncStorage has been extracted from react-native core']);
 
   return (
-    <UserProvider>
-      <StatusBar style={'dark'} />
-      <Navigation />
-    </UserProvider>
+    <ActionSheetProvider>
+      <UserProvider>
+        <StatusBar style={'dark'} />
+        <Navigation />
+      </UserProvider>
+    </ActionSheetProvider>
   );
 };
 
--- rn-photo/src/components/PostItem.js
+++ rn-photo/src/components/PostItem.js
@@ -1,24 +1,64 @@
-import { View, Text, StyleSheet, useWindowDimensions } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  useWindowDimensions,
+  Pressable,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import { memo } from 'react';
 import ImageSwiper from './ImageSwiper';
 import FastImage from './FastImage';
 import { GRAY, PRIMARY, WHITE } from '../colors';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useUserState } from '../contexts/UserContext';
+import { useActionSheet } from '@expo/react-native-action-sheet';
 
+const ActionSheetOptions = {
+  options: ['삭제', '수정', '취소'],
+  cancelButtonIndex: 2,
+  destructiveButtonIndex: 0,
+};
 const PostItem = memo(({ post }) => {
   const width = useWindowDimensions().width;
+  const [user] = useUserState();
+
+  const { showActionSheetWithOptions } = useActionSheet();
+  const onPressActionSheet = (idx) => {
+    if (idx === 0) {
+      // '삭제'버튼을 클릭했을때 할 내용
+    } else if (idx === 1) {
+      // '수정'을 클릭했을때 할 내용
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        {/* USER */}
-        <FastImage
-          source={{ uri: post.user.photoURL }}
-          style={styles.profilePhoto}
-        />
-        <Text style={styles.nickname}>
-          {post.user.displayName ?? 'nickname'}
-        </Text>
+        <View style={styles.profile}>
+          {/* USER */}
+          <FastImage
+            source={{ uri: post.user.photoURL }}
+            style={styles.profilePhoto}
+          />
+          <Text style={styles.nickname}>
+            {post.user.displayName ?? 'nickname'}
+          </Text>
+        </View>
+        {post.user.uid === user.uid && (
+          <Pressable
+            hitSlop={10}
+            onPress={() =>
+              showActionSheetWithOptions(ActionSheetOptions, onPressActionSheet)
+            }
+          >
+            <MaterialCommunityIcons
+              name="dots-horizontal"
+              size={24}
+              color={GRAY.DARK}
+            />
+          </Pressable>
+        )}
       </View>
 
       <View style={{ width, height: width }}>
@@ -57,8 +97,13 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
     paddingHorizontal: 10,
     paddingBottom: 10,
+  },
+  profile: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   profilePhoto: {
     width: 40,
