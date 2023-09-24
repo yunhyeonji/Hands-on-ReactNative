87f0e078b6de64017e0e58d5f02038979b56d2fe
DESKTOP-VCB2MUO\owner <yhj@danusys.com>
2023-09-25 08:59:19 +0900

    맵 화면 만들기

--- rn-photo/src/screens/MapScreen.js
+++ rn-photo/src/screens/MapScreen.js
@@ -1,10 +1,28 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { WHITE } from '../colors';
+import MapView from 'react-native-maps';
+import LocationSearch from '../components/LocationSearch';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const MapScreen = () => {
+  const { top } = useSafeAreaInsets();
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>MapScreen</Text>
+      <MapView style={styles.map} />
+
+      <LocationSearch
+        styles={{
+          container: {
+            ...styles.location,
+            top: top,
+          },
+        }}
+        iconVisible={false}
+        onPress={(data) => {
+          console.log(data);
+        }}
+      />
     </View>
   );
 };
@@ -12,12 +30,17 @@ const MapScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: WHITE,
   },
-  title: {
-    fontSize: 30,
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+  location: {
+    position: 'absolute',
+    width: '90%',
+    borderBottomWidth: 0,
   },
 });
 
