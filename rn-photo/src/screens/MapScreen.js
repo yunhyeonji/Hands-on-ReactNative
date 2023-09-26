3c1950e12475641cb326fc1f9055175cc8d83050
DESKTOP-VCB2MUO\owner <yhj@danusys.com>
2023-09-27 08:44:33 +0900

    지도에 위치표시하기

--- rn-photo/src/screens/MapScreen.js
+++ rn-photo/src/screens/MapScreen.js
@@ -1,15 +1,28 @@
 import { View, StyleSheet } from 'react-native';
 import { WHITE } from '../colors';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import LocationSearch from '../components/LocationSearch';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useState } from 'react';
 
 const MapScreen = () => {
   const { top } = useSafeAreaInsets();
 
+  const [location, setLocation] = useState({
+    latitudeDelta: 0.1,
+    longitudeDelta: 0.1,
+  });
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        style={styles.map}
+        region={location.latitude && location.longitude ? location : null}
+      >
+        {location.latitude && location.longitude && (
+          <Marker coordinate={location} title={location.name} />
+        )}
+      </MapView>
 
       <LocationSearch
         styles={{
@@ -19,8 +32,19 @@ const MapScreen = () => {
           },
         }}
         iconVisible={false}
-        onPress={(data) => {
-          console.log(data);
+        onPress={(data, detail) => {
+          const {
+            geometry: {
+              location: { lat, lng },
+            },
+          } = detail;
+
+          setLocation((prev) => ({
+            ...prev,
+            latitude: lat,
+            longitude: lng,
+            name: data.description,
+          }));
         }}
       />
     </View>
