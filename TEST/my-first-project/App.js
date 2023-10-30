b266d7d31d8b495ca773132c3ef2750f69b4c885
DESKTOP-VCB2MUO\owner <yhj@danusys.com>
2023-10-30 16:58:16 +0900

    header없는 화면 실행해보기

--- TEST/my-first-project/App.js
+++ TEST/my-first-project/App.js
@@ -1,7 +1,9 @@
 import App from "./src/App";
 import Navigations from "./src/Navigations";
+import HeaderlessScreen from "./src/screens/HeaderlessScreen";
 
 // export default App;
-export default Navigations;
+// export default Navigations;
+export default HeaderlessScreen;
 
 // [ eas build -p android --profile preview ] 명령어 작성 하면 apk 생성됨.
