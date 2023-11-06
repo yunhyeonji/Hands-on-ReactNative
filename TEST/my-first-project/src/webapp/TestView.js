b913059988ec0701fc84b21f623eb5e49180edb4
DESKTOP-VCB2MUO\owner <yhj@danusys.com>
2023-11-06 17:59:49 +0900

    흔들기 기능 라이브러리 설치

--- TEST/my-first-project/src/webapp/TestView.js
+++ TEST/my-first-project/src/webapp/TestView.js
@@ -1,9 +1,25 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import { WebView } from "react-native-webview";
 import { Button, StyleSheet, View } from "react-native";
+import { DeviceMotion } from "expo-sensors";
 
 export default function TestView({ navigation }) {
   const webViewRef = useRef(null);
+
+  // 흔들기 기능
+  useEffect(() => {
+    const subscription = DeviceMotion.addListener((data) => {
+      // 여기서 흔들기 이벤트를 처리
+      if (data.acceleration && data.acceleration.x > 10) {
+        // 흔들기 이벤트가 발생하면 원하는 동작 수행
+        console.log("기기가 흔들렸습니다!");
+      }
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
 
   // 웹뷰에서 이벤트 받기
   onWebviewMessage = (e) => {
